7cfe8311c981a8c47639635edf5dc7a081aed575
norvin-jit <norvin@itjones.com>
2025-09-24 16:01:54 -0700

    Add Jones IT trust center

---
+++ constants/trustCenterRegistry/jones-it.js
@@ -0,0 +1,6 @@
+export default {
+  "name": "Jones IT",
+  "trustCenter": "trust.itjones.com",
+  "description": "MSP experts providing a variety of IT-related services and technical support to corporate clients.",
+  "iconUrl": "https://media.licdn.com/dms/image/v2/D4D0BAQEp2kxPvIoVpw/company-logo_200_200/B4DZVc8ZcIHIAM-/0/1741021092166/jonesitsf_logo?e=2147483647&v=beta&t=cET56rxxtzrKbMD2uXwWGuITiJRoknZGDtuAsJYFTJM"
+};
