c94b998fc8055b3ae8c3f90b3ff74d48e4d394c0
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 21:08:43 -0700

    Create safebase.js

---
+++ constants/trustCenterRegistry/safebase.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Safebase",
+  "website": "https://safebase.io/",
+  "trustCenter": "https://trust.safebase.io/",
+  "description": "SafeBase automates security reviews and transforms how you communicate your security and trust posture. Build customer trust through transparency.",
+  "iconUrl": "https://pbs.twimg.com/profile_images/1371557872039985153/1FOMWpp9_400x400.png"
+}
