6e0002ad4e492706c670c5bb2e672f9435f0cbf7
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 20:32:08 -0700

    Update github.js

--- constants/trustCenterRegistry/github.js
+++ constants/trustCenterRegistry/github.js
@@ -1,7 +1,7 @@
 export default {
   "name": "GitHub",
   "website": "https://github.com",
-  "trustCenter": "https://github.com/security",
+  "trustCenter": "https://github.com/trust-center",
   "description": "World's leading software development platform with enterprise-grade security",
   "iconUrl": "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
 }
