8d1292420c8cee60582673d752dadab73c17d80b
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 21:18:59 -0700

    Create eudia.js

---
+++ constants/trustCenterRegistry/eudia.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Eudia",
+  "website": "https://eudia.com/",
+  "trustCenter": "https://trust.eudia.com/",
+  "description": "Eudia is an legal technology company that provides an augmented intelligence platform and legal services to corporate legal departments",
+  "iconUrl": "https://trust.eudia.com/api/share/12e2a8c9-ab2f-4d0e-bbba-098ca561c1a1/logo.png?version=20"
+}
