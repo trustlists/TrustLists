4ec9cbe75653cb86792419a092fa4281ef6b5634
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 21:22:23 -0700

    Create figma.js

---
+++ constants/trustCenterRegistry/figma.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Figma",
+  "website": "https://figma.com/",
+  "trustCenter": "https://compliance.figma.com/",
+  "description": "Figma is a cloud-based collaborative interface design tool used to create digital products like websites, mobile apps, and other experiences.",
+  "iconUrl": "https://cdn.sanity.io/images/599r6htc/regionalized/5094051dac77593d0f0978bdcbabaf79e5bb855c-1080x1080.png?w=540&h=540&q=75&fit=max&auto=format"
+}
