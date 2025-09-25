359f36c31b7d7892fe0106a1844fbbd08df5bb81
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 21:52:33 -0700

    Create slack.js

---
+++ constants/trustCenterRegistry/slack.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Slack",
+  "website": "https://slack.com/",
+  "trustCenter": "https://slack.com/trust",
+  "description": "BSlack is a work operating system and collaboration platform that functions as a central hub for team communication, file sharing, and project management.",
+  "iconUrl": "https://1000logos.net/wp-content/uploads/2021/06/Slack-logo.png"
+}
