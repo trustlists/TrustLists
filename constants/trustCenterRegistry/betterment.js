e44fd0bd417b20aacc75665e850153eddbd10251
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 21:33:40 -0700

    Create betterment.js

---
+++ constants/trustCenterRegistry/betterment.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Betterment",
+  "website": "https://betterment.com/",
+  "trustCenter": "https://trust.betterment.com/",
+  "description": "Betterment is an internet-based advisory services are designed to assist clients in achieving discrete financial goals.",
+  "iconUrl": "https://api.conveyor.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaEpJaWt6TWpSaVpUaGtaaTB6TWpVMExUUmhNVEV0T1RWaU1pMW1PVEF6TVRCaVlqVTROVEFHT2daRlZBPT0iLCJleHAiOm51bGwsInB1ciI6ImJsb2JfaWQifX0=--c3b6135fd1eb931d98d304dae8cc4cabb91c4a6d/betterment.png"
+}
