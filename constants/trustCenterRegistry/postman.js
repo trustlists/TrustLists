04772deb8cdc7aac0bce15fa0402e2e9ce1b5ad4
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 21:46:48 -0700

    Create postman.js

---
+++ constants/trustCenterRegistry/postman.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Postman",
+  "website": "https://postman.com/",
+  "trustCenter": "https://security.postman.com/",
+  "description": "Postman is an all-in-one API platform that streamlines the entire API lifecycle by providing a comprehensive set of tools for design, testing, and collaboration.",
+  "iconUrl": "https://security.postman.com/api/share/3299ad14-25cc-4065-acdf-0e01a16a852d/logo.png?version=33"
+}
