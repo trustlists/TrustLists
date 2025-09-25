34a9b7afbd2f8d6b0be1656e988dc98a62b33132
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 20:50:01 -0700

    Create superhuman.js

---
+++ constants/trustCenterRegistry/superhuman.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Superhuman",
+  "website": "https://superhuman.com",
+  "trustCenter": "https://trust.superhuman.com", // ← Change this
+  "description": "Superhuman is the most productive email app ever made. Collaborate faster and get more done with AI-native email.",
+  "iconUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ50G52axTpCjQ9vhdS8epCr7uIQ7nEXgb0OA&s"
+}
