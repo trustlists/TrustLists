56f9618f829db474557885d4e13a4fcc9473012d
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 22:04:24 -0700

    Update cursor.js

--- constants/trustCenterRegistry/cursor.js
+++ constants/trustCenterRegistry/cursor.js
@@ -3,5 +3,5 @@ export default {
   "website": "https://cursor.com/",
   "trustCenter": "https://trust.cursor.com/",
   "description": "Cursor AI is a proprietary, AI-native code editor built on top of the open-source codebase.",
-  "iconUrl": "hhttps://trust.cursor.com/doc?s=8oae3i9p56u0y9k26580m4"
+  "iconUrl": "https://trust.cursor.com/doc?s=8oae3i9p56u0y9k26580m4"
 }
