d6a6cfca80b13ca4ef39dd3696b2f106782ad59b
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 22:33:01 -0700

    Create checkr.js

---
+++ constants/trustCenterRegistry/checkr.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Checkr",
+  "website": "https://checkr.com/",
+  "trustCenter": "https://security.checkr.com/",
+  "description": "Checkr is an technology platform for background checks that helps companies and individuals streamline employment screenings and access public records",
+  "iconUrl": "https://security.checkr.com/api/share/85bfddea-ddb4-4498-872f-51817932f6a4/logo.png?version=24"
+}
