38bce6f892c37fc6e39fe7196bf2e3325b89c4d8
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 20:58:49 -0700

    Update salesforce.js

--- constants/trustCenterRegistry/salesforce.js
+++ constants/trustCenterRegistry/salesforce.js
@@ -3,5 +3,5 @@ export default {
   "website": "https://salesforce.com",
   "trustCenter": "https://trust.salesforce.com",
   "description": "Leading CRM platform with comprehensive trust and compliance programs",
-  "iconUrl": "https://www.salesforce.com/content/dam/web/en_us/www/images/nav/salesforce-cloud-logo-v2.png"
+  "iconUrl": "https://www.salesforce.com/news/wp-content/uploads/sites/3/2021/05/Salesforce-logo.jpg?w=2048&h=1152"
 }
