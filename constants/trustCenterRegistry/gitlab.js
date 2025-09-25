80760621899c42ab50bc157ccfdf23d77892d8af
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 21:44:59 -0700

    Create gitlab.js

---
+++ constants/trustCenterRegistry/gitlab.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Gitlab",
+  "website": "https://gitlab.com/",
+  "trustCenter": "https://trust.gitlab.com/",
+  "description": "GitLab is an AI-powered, unified DevSecOps platform that helps teams manage the entire software development lifecycle, from planning and source code management to automated CI/CD, security testing, and deployment.",
+  "iconUrl": "https://trust.gitlab.com/api/share/e3f22038-1c1b-4cd0-8ebe-7e95017bec60/logo.png?version=35"
+}
