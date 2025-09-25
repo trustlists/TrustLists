5c03c3ab2bcfc9c1dbaf8ac2cc2752174f49219a
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 20:59:30 -0700

    Update stripe.js

--- constants/trustCenterRegistry/stripe.js
+++ constants/trustCenterRegistry/stripe.js
@@ -3,5 +3,5 @@ export default {
   "website": "https://stripe.com",
   "trustCenter": "https://stripe.com/privacy-center/legal",
   "description": "Online payment processing platform with comprehensive security and compliance programs",
-  "iconUrl": "https://stripe.com/img/v3/home/social.png"
+  "iconUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQGluJhW7I1NYU7jF77E-9K9I46_ib_DUNHw&s"
 }
