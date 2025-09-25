e0c1164906463213e08b6d8611bdd166aca3b95e
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 22:16:35 -0700

    Create delve.js

---
+++ constants/trustCenterRegistry/delve.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Delve",
+  "website": "https://delve.co/",
+  "trustCenter": "https://trust.delve.co/",
+  "description": "Delve is in compliance with security best practices, has implemented and is monitoring comprehensive controls, and maintains policies to outline its security procedures.",
+  "iconUrl": "https://mma.prnewswire.com/media/2735501/DelveBlackLogo__002_Logo.jpg?p=facebook"
+}
