7ae43001b7a9d6a8cf28f858523072cc11eb1229
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 21:40:44 -0700

    Create vercel.js

---
+++ constants/trustCenterRegistry/vercel.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Vercel",
+  "website": "https://vercel.com/",
+  "trustCenter": "https://security.vercel.com/",
+  "description": "Vercel is a cloud platform for frontend developers that simplifies the deployment, hosting, and scaling of modern web applications.",
+  "iconUrl": "https://security.vercel.com/api/share/045f15d5-2260-44d8-aa72-de6a8a3dbff8/logo.png?version=128"
+}
