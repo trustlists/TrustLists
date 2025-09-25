a752d8f402d5f115ae656afffdf0e5af8fdf4bfa
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 20:34:45 -0700

    Create kandji.js

---
+++ constants/trustCenterRegistry/kandji.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Kandji",
+  "website": "https://www.kandji.io",
+  "trustCenter": "https://www.trust.kandji.io",
+  "description": "Kandji is a cloud-based Apple device management and security platform designed to centralize and automate the configuration, management, and security of Mac, iPhone, iPad, and Apple TV devices within an organization",
+  "iconUrl": "hhttps://images.credly.com/images/e248567c-552c-4c58-abc7-fb9ef1157f1a/blob.png"
+}
