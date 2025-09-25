169e27010202661ed86e983f42fdd9b9bf6c6c0f
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-24 21:10:52 -0700

    Create drata.js

---
+++ constants/trustCenterRegistry/drata.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Drata",
+  "website": "https://drata.com/",
+  "trustCenter": "https://trust.drata.com/",
+  "description": "Drata is a security and compliance automation platform that helps companies streamline compliance, manage risk, and build trust.",
+  "iconUrl": "https://drata.com/images/favicon-256x256.png"
+}
