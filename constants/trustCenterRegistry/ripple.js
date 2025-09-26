b418944646fbe0f5254492d3c141ea812e69ec97
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-26 00:37:25 -0700

    Create ripple.js

---
+++ constants/trustCenterRegistry/ripple.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Ripple",
+  "website": "https://ripple.com/",
+  "trustCenter": "https://security-portal.ripple.com/",
+  "description": "Ripple is an enterprise crypto company that enables financial institutions, businesses, governments and developers to drive impact and economic opportunity.",
+  "iconUrl": "https://www.google.com/s2/favicons?domain=ripple.com&sz=128"
+};
