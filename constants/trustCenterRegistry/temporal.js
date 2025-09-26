0195de381a6b7fac8a6b35d2b2f7b7e35496185f
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-25 22:27:56 -0700

    Create temporal.js

---
+++ constants/trustCenterRegistry/temporal.js
@@ -0,0 +1,7 @@
+export default {
+"name": "Temporal",
+"website": "https://temporal.io/",
+"trustCenter": "https://trust.temporal.io/",
+"description": "Temporal.io is an open-source durable execution system that provides a workflow engine to build reliable, scalable distributed applications by managing complex state and coordinating microservices.",
+"iconUrl": "https://www.google.com/s2/favicons?domain=temporal.io&sz=128"
+}
