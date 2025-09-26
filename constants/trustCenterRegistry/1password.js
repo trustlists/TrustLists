9f7660323e88c888f8f9063543039bd200e2086b
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-25 19:52:55 -0700

    Update 1password.js

--- constants/trustCenterRegistry/1password.js
+++ constants/trustCenterRegistry/1password.js
@@ -4,4 +4,4 @@ export default {
 "trustCenter": "https://app.conveyor.com/profile/1password",
 "description": "1Password is an industry-leading digital vault and password manager that helps individuals, families, and businesses securely store, organize, and manage all their sensitive information, including passwords, credit card details, and secure notes.",
 "iconUrl": "https://media.licdn.com/dms/image/v2/D4E0BAQETtEKm8EaUZA/company-logo_400_400/company-logo_400_400/0/1688384038900/1password_logo?e=2147483647&v=beta&t=X4HwxaThddp98lyiHBg9fUgCzhbJ-8hf28BGnfuB4eI"
-};
+}
