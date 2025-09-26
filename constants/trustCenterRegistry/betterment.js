de7defd13e2a7d42b7988e4eff274edc6375ae92
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-26 00:59:44 -0700

    Update betterment.js

--- constants/trustCenterRegistry/betterment.js
+++ constants/trustCenterRegistry/betterment.js
@@ -2,6 +2,6 @@ export default {
   "name": "Betterment",
   "website": "https://betterment.com/",
   "trustCenter": "https://trust.betterment.com/",
-  "description": "Betterment is an internet-based advisory services are designed to assist clients in achieving discrete financial goals.",
+  "description": "Betterment is an advisory services are designed to assist clients in achieving discrete financial goals.",
   "iconUrl": "https://www.google.com/s2/favicons?domain=betterment.com&sz=128"
 };
