d2a5b868b481915ae6d5272aa811ecb210cad0d2
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-26 14:46:15 -0700

    Create secureframe.js

---
+++ constants/trustCenterRegistry/secureframe.js
@@ -0,0 +1,8 @@
+export default {
+  "name": "Secureframe",
+  "website": "https://secureframe.com/",
+  "trustCenter": "https://trust.secureframe.com/",
+  "description": "Secureframe is an all-in-one platform that automates security compliance and audit readiness for businesses by collecting evidence, monitoring security, and managing risk across various frameworks like SOC 2, ISO 27001, and FedRAMP.",
+  "platform": "Secureframe",
+  "iconUrl": "https://www.google.com/s2/favicons?domain=secureframe.com&sz=128"
+};
