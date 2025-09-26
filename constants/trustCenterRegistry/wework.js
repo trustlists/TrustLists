fe8c692a102f0a04f5b19fccb1ca1af9e47eaedf
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-26 14:55:36 -0700

    Create wework.js

---
+++ constants/trustCenterRegistry/wework.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Wework",
+  "website": "https://wework.com/",
+  "trustCenter": "https://trust.wework.com/",
+  "description": "WeWork is a global real estate company that provides flexible workspace solutions, such as coworking spaces and private offices, for businesses and individuals in major cities worldwide.",
+  "iconUrl": "https://www.google.com/s2/favicons?domain=wework.com&sz=128"
+};
