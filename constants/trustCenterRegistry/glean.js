582318f6aa61262f8544a4f02dd01d315c3f4f64
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-27 22:52:31 -0700

    Create glean.js

---
+++ constants/trustCenterRegistry/glean.js
@@ -0,0 +1,7 @@
+export default {
+  "name": "Glean",
+  "website": "https://www.glean.com/",
+  "trustCenter": "https://trust.glean.com/",
+  "description": "Glean is an AI-powered enterprise work assistant and search platform designed to help employees find information, generate content, and automate workflows across all of their company's applications.",
+  "iconUrl": "https://www.google.com/s2/favicons?domain=glean.com&sz=128"
+};
