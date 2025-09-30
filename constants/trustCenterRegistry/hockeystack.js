625f416ef0e2c032bdb739556c53835eceeaae0a
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-29 22:07:36 -0700

    Create hockeystack.js

---
+++ constants/trustCenterRegistry/hockeystack.js
@@ -0,0 +1,6 @@
+export default {
+  "name": "HockeyStack",
+  "website": "https://www.hockeystack.com/",
+  "trustCenter": "https://security.hockeystack.com/",
+  "iconUrl": "https://www.google.com/s2/favicons?domain=hockeystack.com&sz=128"
+};
