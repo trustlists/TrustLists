96576c3f9908ff3c3047d63a3af4afdcf1482a4d
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-29 22:10:16 -0700

    Create bland_ai.js

---
+++ constants/trustCenterRegistry/bland_ai.js
@@ -0,0 +1,6 @@
+export default {
+  "name": "Bland",
+  "website": "https://www.bland.ai/",
+  "trustCenter": "https://trust.delve.co/blandai",
+  "iconUrl": "https://www.google.com/s2/favicons?domain=bland.ai&sz=128"
+};
