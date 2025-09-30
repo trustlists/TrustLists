b9a6328300c06c064b6ec65f100d845a3c0ef624
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-09-29 22:11:14 -0700

    Update hockeystack.js

--- constants/trustCenterRegistry/hockeystack.js
+++ constants/trustCenterRegistry/hockeystack.js
@@ -1,6 +1,6 @@
 export default {
   "name": "HockeyStack",
   "website": "https://www.hockeystack.com/",
-  "trustCenter": "https://security.hockeystack.com/",
+  "trustCenter": "https://trust.delve.co/hockeystack",
   "iconUrl": "https://www.google.com/s2/favicons?domain=hockeystack.com&sz=128"
 };
