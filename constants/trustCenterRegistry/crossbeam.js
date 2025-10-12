5a0d3df4763e3413cb0807ea40d90dc02067ab93
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-10-12 10:50:46 -0700

    Create crossbeam.js

---
+++ constants/trustCenterRegistry/crossbeam.js
@@ -0,0 +1,6 @@
+export default {
+"name": "Crossbeam",
+"website": "https://www.crossbeam.com/",
+"trustCenter": "https://security.crossbeam.com/",
+"iconUrl": "https://www.google.com/s2/favicons?domain=crossbeam.com&sz=128"
+};
