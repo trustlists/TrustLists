a4275eecc2111c29760ac8bc12a8d3321097ca21
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-10-16 14:57:54 -0700

    Create tidio.js

---
+++ constants/trustCenterRegistry/tidio.js
@@ -0,0 +1,6 @@
+export default {
+  "name": "Tidio",
+  "website": "https://www.tidio.com/",
+  "trustCenter": "https://trust.tidio.com/",
+  "iconUrl": "https://www.google.com/s2/favicons?domain=tidio.com&sz=128"
+};
