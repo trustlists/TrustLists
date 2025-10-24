707f194794a1fad1b05e9abe21c0d1c118a304d8
Michael <michael@Michaels-MacBook-Pro.local>
2025-10-23 23:49:06 -0700

    Fix missing quote in iru.js

--- constants/trustCenterRegistry/iru.js
+++ constants/trustCenterRegistry/iru.js
@@ -1,7 +1,6 @@
-
 export default {
   "name": "iru",
   "website": "https://www.iru.com/",
-  "trustCenter": "https://trust.iru.com/,
+  "trustCenter": "https://trust.iru.com/",
   "iconUrl": "https://www.google.com/s2/favicons?domain=iru.com&sz=128"
 };
