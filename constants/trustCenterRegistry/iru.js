b98a0478ac05649c4ace4eabb317dffbe370af10
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-10-23 23:44:21 -0700

    Create iru.js

---
+++ constants/trustCenterRegistry/iru.js
@@ -0,0 +1,7 @@
+
+export default {
+  "name": "iru",
+  "website": "https://www.iru.com/",
+  "trustCenter": "https://trust.iru.com/,
+  "iconUrl": "https://www.google.com/s2/favicons?domain=iru.com&sz=128"
+};
