77eacc96dad2ab76d1fbc1ea7439f20cb151a709
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2025-10-27 20:16:16 -0700

    Update iru.js

--- constants/trustCenterRegistry/iru.js
+++ constants/trustCenterRegistry/iru.js
@@ -3,5 +3,5 @@ export default {
   "website": "https://www.iru.com/",
   "trustCenter": "https://trust.iru.com/",
   "platform": "Iru",
-  "iconUrl": "https://img.logo.dev/iru.com?token=pk_X-1ZBzhHScuPdjGlT-1rrQ"
+  "iconUrl": "https://www.iru.com/favicon.ico"
 };
